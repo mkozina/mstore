ac35323f28062c850e73763281428ffda93ee437
mkozina <mkozina@sigma.ug.edu.pl>
2015-11-16 18:47:52 +0100

    modyfikacje plików klas

--- src/main/java/mkozina/mstore/storage/DataBase.java
+++ src/main/java/mkozina/mstore/storage/DataBase.java
@@ -9,11 +9,28 @@ public class DataBase {
 	
 	private List<CompactDisc> db = new ArrayList<CompactDisc>();
 	
+	// dodawanie obiektu do listy
 	public void add(CompactDisc compactDisc){
 		CompactDisc newCompactDisc = new CompactDisc(compactDisc.getArtist(), compactDisc.getAlbum(), compactDisc.getYear(), compactDisc.getTracks());
 		db.add(newCompactDisc);
 	}
 	
+	// modyfikowanie istniejacego obiektu w liscie
+	// pierwszy parametr wskazuje na indeks obiektu w liscie
+	// drugi parametr to obiekt, z ktorego pobieramy nowe informacje
+	public void modify(String index, CompactDisc compactDisc) {
+		int indexInt = Integer.parseInt(index);
+		db.set(indexInt, new CompactDisc(compactDisc.getArtist(), compactDisc.getAlbum(), compactDisc.getYear(), compactDisc.getTracks()));
+	}
+	
+	// usuwanie istniejacego obiektu z listy
+	// parametr wskazuje na indeks obiektu w liscie
+	public void delete(String index) {
+		int indexInt = Integer.parseInt(index);
+		db.remove(indexInt);
+	}
+	
+	// pobranie calej listy
 	public List<CompactDisc> getAllCompactDiscs(){
 		return db;
 	}
